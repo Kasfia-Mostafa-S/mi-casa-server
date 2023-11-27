6e1c55e60c20b816f620f1575a6e6b6e25deed93
SwornaCode <kasfiamostafa03@gmail.com>
2023-11-27 20:57:02 +0600

    Manage review added

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+var jwt = require("jsonwebtoken");
 const cors = require("cors");
-
-
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ijwgr8d.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,89 +27,213 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
+    const userCollection = client.db("miCasaDB").collection("users");
     const propertyCollection = client.db("miCasaDB").collection("properties");
     const reviewCollection = client.db("miCasaDB").collection("review");
     const wishesCollection = client.db("miCasaDB").collection("wishes");
 
+    // jwt api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    // middleware
+    const verifyToken = (req, res, next) => {
+      console.log("Inside verify token", req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "Forbidden access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN, (error, decoded) => {
+        if (error) {
+          return res.status(401).send({ message: "Forbidden access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    // user related
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exist", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
 
 
-// all properties
-app.get("/property", async (req, res) => {
-  const page = parseInt(req.query.page);
-  const size = parseInt(req.query.size);
-  const cursor = propertyCollection.find();
-  const result = await cursor
-    .skip(page * size)
-    .limit(size)
-    .toArray();
-  res.send(result);
-});
-app.get("/propertyCount", async (req, res) => {
-  const count = await propertyCollection.estimatedDocumentCount();
-  res.send({ count });
-});
+    // admin selection
+    app.get("/users/admin/:email",verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "Forbidden access" });
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
+    });
 
-app.get("/property/:id", async (req, res) => {
-  const id = req.params.id;
-  const query = { _id: new ObjectId(id) };
-  const result = await propertyCollection.findOne(query);
-  res.send(result);
-});
+    app.patch(
+      "/users/admin/:id",
+      async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updatedDoc = {
+          $set: {
+            role: "admin",
+          },
+        };
+        const result = await userCollection.updateOne(filter, updatedDoc);
+        res.send(result);
+      }
+    );
 
-
-// review
-app.post("/review", async (req, res) => {
-  const propertyItem = req.body;
-  const result = await reviewCollection.insertOne(propertyItem);
-  res.send(result);
-});
-
-app.get("/review", async (req, res) => {
-  const result = await reviewCollection.find().toArray();
-  res.send(result);
-});
-
-app.get("/review/:title", async (req, res) => {
-  const title = req.params.title;
-  const query = { title: title };
-  const result = await reviewCollection.find(query).toArray();
-  res.send(result);
-});
-
-// add to wishlist
-app.post("/wishList", async (req, res) => {
-  const propertyWish = req.body;
-  const result = await wishesCollection.insertOne(propertyWish);
-  res.send(result);
-});
-
-app.get("/wishList", async (req, res) => {
-  const email = req.query.email;
+ // admin selection
+ app.get("/users/agent/:email",verifyToken, async (req, res) => {
+  const email = req.params.email;
+  if (email !== req.decoded.email) {
+    return res.status(403).send({ message: "Forbidden access" });
+  }
   const query = { email: email };
-  const result = await wishesCollection.find(query).toArray();
-  res.send(result);
+  const user = await userCollection.findOne(query);
+  let agent = false;
+  if (user) {
+    agent = user?.role === "agent";
+  }
+  res.send({ agent });
 });
 
-app.get("/wishList/:id", async (req, res) => {
-  const id = req.params.id;
-  const query = { _id: new ObjectId(id) };
-  const result = await wishesCollection.findOne(query);
-  res.send(result);
-});
+app.patch(
+  "/users/agent/:id",
+  async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) };
+    const updatedDoc = {
+      $set: {
+        role: "agent",
+      },
+    };
+    const result = await userCollection.updateOne(filter, updatedDoc);
+    res.send(result);
+  }
+);
 
-app.delete("/:id", async (req, res) => {
-  const id = req.params.id;
-  const query = { _id: new ObjectId(id) };
-  const result = await wishesCollection.deleteOne(query);
-  res.send(result);
-});
+    // all properties
+    app.get("/property", async (req, res) => {
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      const cursor = propertyCollection.find();
+      const result = await cursor
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+      res.send(result);
+    });
 
+    app.get("/propertyCount", async (req, res) => {
+      const count = await propertyCollection.estimatedDocumentCount();
+      res.send({ count });
+    });
+
+    app.get("/property/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await propertyCollection.findOne(query);
+      res.send(result);
+    });
+
+    // review
+    app.post("/review", async (req, res) => {
+      const propertyItem = req.body;
+      const result = await reviewCollection.insertOne(propertyItem);
+      res.send(result);
+    });
+
+    app.get("/review", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/review/:title", async (req, res) => {
+      const title = req.params.title;
+      const query = { title: title };
+      const result = await reviewCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/review", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await reviewCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete("/review/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await reviewCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // add to wishlist
+    app.post("/wishlist", async (req, res) => {
+      const propertyWish = req.body;
+      const result = await wishesCollection.insertOne(propertyWish);
+      res.send(result);
+    });
+
+    app.get("/wishlist", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await wishesCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/wishlist/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await wishesCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.delete("/wishlist/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await wishesCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    // Ensures that the client will close when you finish/error
+    // Ensures that the client will close when you finish/error0
     // await client.close();
   }
 }
